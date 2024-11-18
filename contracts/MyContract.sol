d5ad80cab708e9d5ee2763822d9aee060b70ae98
codingvaleria <codingvaleria@gmail.com>
2024-11-18 12:24:46 +0300

    Fist commit with Solidity Contract

---
+++ contracts/MyContract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract MyContract{
+    uint public number;
+
+    function setNumber(uint_number) public{
+        number = _number;
+    }
+    }
+}
